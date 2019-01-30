7599b31297f4db8f381505f6d602d08042364d98
Frederic Lhoest <flhoest@gmail.com>
2019-01-30 16:06:57 +0100

    Add files via upload

---
+++ nxCredentials.php
@@ -0,0 +1,35 @@
+<?php
+
+// MSDS Platform
+
+// 	$clusterConnect=array(
+// 					"username" => "flhoest",
+// 					"password" => "DoNotChangeMe!",
+// 					"ip" => "10.57.26.90"
+// 					);
+
+// Architecture Platform
+
+	$clusterConnect=array(
+					"username" => "flhoest",
+					"password" => "DoNotChangeMe!",
+					"ip" => "192.168.8.189"
+					);
+
+// Nutanix UK
+
+// 	$clusterConnect=array(
+// 					"username" => "flhoest",
+// 					"password" => "DoNotChangeMe!",
+// 					"ip" => "172.31.248.19"
+// 					);
+
+// Nutanix HK CE
+
+// 	$clusterConnect=array(
+// 					"username" => "flhoest",
+// 					"password" => "DoNotChangeMe!",
+// 					"ip" => "192.168.81.76"
+// 					);
+
+?>
