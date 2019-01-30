cf8e36521e8f8eea373460b1f8b70b8f57491886
Frederic Lhoest <flhoest@gmail.com>
2019-01-30 16:07:59 +0100

    Update nxCredentials.php

--- nxCredentials.php
+++ nxCredentials.php
@@ -1,35 +1,11 @@
 <?php
 
-// MSDS Platform
-
-// 	$clusterConnect=array(
-// 					"username" => "flhoest",
-// 					"password" => "DoNotChangeMe!",
-// 					"ip" => "10.57.26.90"
-// 					);
-
-// Architecture Platform
+// My Nutanix Cluster details
 
 	$clusterConnect=array(
-					"username" => "flhoest",
-					"password" => "DoNotChangeMe!",
-					"ip" => "192.168.8.189"
-					);
-
-// Nutanix UK
-
-// 	$clusterConnect=array(
-// 					"username" => "flhoest",
-// 					"password" => "DoNotChangeMe!",
-// 					"ip" => "172.31.248.19"
-// 					);
-
-// Nutanix HK CE
-
-// 	$clusterConnect=array(
-// 					"username" => "flhoest",
-// 					"password" => "DoNotChangeMe!",
-// 					"ip" => "192.168.81.76"
-// 					);
+				"username" => "username",
+				"password" => "password",
+				"ip" => "192.168.x.x"
+				);
 
 ?>
